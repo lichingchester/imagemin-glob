52a4df6ce5f9c40987dc379af1d86c944f6326b0
lichingchester <chester.li@onepage.com.hk>
2019-06-27 19:36:09 +0800

    v3.0.0

--- src.old/config.js
+++
@@ -1,4 +0,0 @@
-export const DEBUG = false;
-export const PATH_ORI_IMAGES = './images';
-export const PATH_MIN_IMAGES = './build';
-export const DEFAULT_QUALITY = '0.8';
--- src.old/fse.js
+++
@@ -1,6 +0,0 @@
-import fse from 'fs-extra';
-
-export default async function isExists(path) {
-  const exists = await fse.pathExists(path);
-  return exists;
-}
